1a33f61b8982112de54fcdfd1a23c3f0aa1cad88
celikyus01 <62037357+celikyus01@users.noreply.github.com>
2022-04-22 23:21:37 +0100

    Initial commit

---
+++ src/test/java/com/acme/coreBanking/practice/practice1.java
@@ -0,0 +1,23 @@
+package com.acme.coreBanking.practice;
+
+import com.acme.coreBanking.utils.ConfigurationReader;
+import com.acme.coreBanking.utils.Driver;
+import org.openqa.selenium.WebDriver;
+
+public class practice1 {
+
+    public static void main(String[] args) {
+        WebDriver driver = Driver.get();
+        driver.get(ConfigurationReader.get("url"));
+        System.out.println("what is the name= "+ ConfigurationReader.get("name"));
+
+        driver.manage().window().maximize();
+        Driver.closeDriver();
+    }
+
+
+
+
+
+
+}
