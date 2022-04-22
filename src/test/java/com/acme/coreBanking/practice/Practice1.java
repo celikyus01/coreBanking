7bde01bbe6c19980fa1f190d77059e85687a09d4
celikyus01 <62037357+celikyus01@users.noreply.github.com>
2022-04-23 00:47:08 +0100

    first commit

--- src/test/java/com/acme/coreBanking/practice/Practice1.java
+++ src/test/java/com/acme/coreBanking/practice/Practice1.java
@@ -2,18 +2,43 @@ package com.acme.coreBanking.practice;
 
 import com.acme.coreBanking.utils.ConfigurationReader;
 import com.acme.coreBanking.utils.Driver;
+import org.junit.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Practice1 {
 
-    public static void main(String[] args) {
-        WebDriver driver = Driver.get();
-        driver.get(ConfigurationReader.get("url"));
-        System.out.println("what is the name= "+ ConfigurationReader.get("name"));
+    WebDriver driver;
 
-        driver.manage().window().maximize();
+    @Before
+    public void setupDriver(){
+        driver=Driver.get();
+    }
+
+    @After
+    public void tearDown(){
         Driver.closeDriver();
     }
+
+    @Test
+    public void test1() throws InterruptedException {
+
+        driver.get(ConfigurationReader.get("url"));
+        Thread.sleep(3000);
+        String actualTitle = driver.getTitle();
+        String expectedTitle="ACME demo app";
+
+        Assert.assertEquals("title mismatch ",expectedTitle,actualTitle);
+
+        String actualCurrentUrl = driver.getCurrentUrl();
+
+        driver.findElement(By.xpath("//span[.='Make Payment']")).click();
+
+
+    }
+
+
+
 
 
 
